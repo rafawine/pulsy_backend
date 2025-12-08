94e462afd68cad8f436bbb3e13fb7ff54f7cb657
Rafael Barrientos García <rafiprogramando@gmail.com>
2025-12-08 16:55:39 -0600

    Revert "Se cambia el nombre del header para obtener el valor del ID del consumidor"
    This reverts commit 3d83166cf965e3e8ee2ee5018fa73e82d14ccbd4.

--- internal/handlers/storage_handler.go
+++ internal/handlers/storage_handler.go
@@ -18,7 +18,7 @@ import (
 )
 
 func UploadFileHandler(c *gin.Context) {
-	uuidOwner := c.Request.Header.Get("X-Consumer-Key")
+	uuidOwner := c.Request.Header.Get("x-consumer-custom-id")
 	file, err := c.FormFile("file")
 	if err != nil {
 		message := "file not found"
@@ -106,7 +106,7 @@ func UploadFileHandler(c *gin.Context) {
 }
 
 func DownloadFileHandler(c *gin.Context) {
-	uuidOwner := c.Request.Header.Get("X-Consumer-Key")
+	uuidOwner := c.Request.Header.Get("x-consumer-custom-id")
 	fileUUID := c.Param("uuid")
 
 	metadata, err := getMetadata(fileUUID, uuidOwner)
@@ -180,7 +180,7 @@ func DownloadFileHandler(c *gin.Context) {
 }
 
 func UpdateFileHandler(c *gin.Context) {
-	uuidOwner := c.Request.Header.Get("X-Consumer-Key")
+	uuidOwner := c.Request.Header.Get("x-consumer-custom-id")
 	fileUUID := c.Param("uuid")
 	file, err := c.FormFile("file")
 	if err != nil {
@@ -286,7 +286,7 @@ func UpdateFileHandler(c *gin.Context) {
 }
 
 func DeleteFileHandler(c *gin.Context) {
-	uuidOwner := c.Request.Header.Get("X-Consumer-Key")
+	uuidOwner := c.Request.Header.Get("x-consumer-custom-id")
 	fileUUID := c.Param("uuid")
 
 	metadata, err := getMetadata(fileUUID, uuidOwner)
